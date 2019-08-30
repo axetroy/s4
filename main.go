0661e3cf562a00b67867e5ddc9120c8c6dafc081
axetroy <troy450409405@gmail.com>
2019-08-30 12:05:02 +0800

    bump v0.4.2

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 
 	app.Name = "s4"
 	app.Usage = "Integrate local and remote workflow"
-	app.Version = "0.4.1"
+	app.Version = "0.4.2"
 	app.Author = "Axetroy"
 	app.Email = "axetroy.dev@gmail.com"
 
