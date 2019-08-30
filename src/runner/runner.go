653b7ef3e23265abd39862623d8154a56f3dfc30
axetroy <troy450409405@gmail.com>
2019-08-30 14:34:56 +0800

    refactor code

--- src/runner/runner.go
+++ src/runner/runner.go
@@ -1,21 +1,25 @@
 package runner
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"github.com/AlecAivazis/survey"
 	"github.com/axetroy/s4/src/configuration"
-	"github.com/axetroy/s4/src/runner/actions"
 	"github.com/axetroy/s4/src/ssh"
+	"github.com/axetroy/s4/src/variable"
 	"github.com/fatih/color"
 	"os"
+	"os/exec"
+	"path"
+	"strings"
 )
 
 type Runner struct {
-	SSH    *ssh.Client
-	Config *configuration.Configuration
-	Step   int    // current step
-	Cwd    string // current working dir at local
+	Config *configuration.Configuration // the configuration
+	SSH    *ssh.Client                  // current ssh client
+	Step   int                          // current step
+	Cwd    string                       // current working dir at local
 }
 
 func NewRunner(configFilepath string) (*Runner, error) {
@@ -47,8 +51,6 @@ func (r *Runner) Run() error {
 	client := ssh.NewSSH(r.Config)
 	r.SSH = client
 
-	r.Step++
-
 	fmt.Printf("[step %v]: CONNECT %s\n", r.Step, color.GreenString(fmt.Sprintf("%s@%s:%s", r.Config.Username, r.Config.Host, r.Config.Port)))
 
 	if r.Config.Password == "" {
@@ -77,69 +79,351 @@ func (r *Runner) Run() error {
 		r.Cwd = cwd
 	}
 
-	remoteCwd, err := client.Pwd()
-
-	if err != nil {
+	if remoteCwd, err := client.Pwd(); err != nil {
 		return err
+	} else {
+		r.Config.CWD = remoteCwd
 	}
-
-	r.Config.CWD = remoteCwd
 
 	for _, action := range r.Config.Actions {
 		r.Step++
 		switch action.Action {
 		case "VAR":
-			if err := actions.Var(r, action); err != nil {
+			if err := r.actionVar(action); err != nil {
 				return err
 			}
 			break
 		case "CD":
-			if err := actions.Cd(r, action); err != nil {
+			if err := r.actionCd(action); err != nil {
 				return err
 			}
 			break
 		case "BASH":
-			if err := actions.Bash(r, action); err != nil {
+			if err := r.actionBash(action); err != nil {
 				return err
 			}
 			break
 		case "CMD":
-			if err := actions.Cmd(r, action); err != nil {
+			if err := r.actionCmd(action); err != nil {
 				return err
 			}
 			break
 		case "RUN":
-			if err := actions.Run(r, action); err != nil {
+			if err := r.actionRun(action); err != nil {
 				return err
 			}
 			break
 		case "MOVE":
-			if err := actions.Move(r, action); err != nil {
+			if err := r.actionMove(action); err != nil {
 				return err
 			}
 			break
 		case "COPY":
-			if err := actions.Copy(r, action); err != nil {
+			if err := r.actionCopy(action); err != nil {
 				return err
 			}
 			break
 		case "DELETE":
-			if err := actions.Delete(r, action); err != nil {
+			if err := r.actionDelete(action); err != nil {
 				return err
 			}
 			break
 		case "UPLOAD":
-			if err := actions.Upload(r, action); err != nil {
+			if err := r.actionUpload(action); err != nil {
 				return err
 			}
 			break
 		case "DOWNLOAD":
-			if err := actions.Download(r, action); err != nil {
+			if err := r.actionDownload(action); err != nil {
 				return err
 			}
 			break
 		default:
 			return errors.New(fmt.Sprintf("Invalid action `%s`", action.Action))
+		}
+	}
+
+	r.Step++
+
+	fmt.Printf("[step %d]: %s", r.Step, color.GreenString("done!"))
+
+	return nil
+}
+
+func (r *Runner) actionBash(action configuration.Action) error {
+	command := strings.Join(action.Arguments, " ")
+
+	fmt.Printf("[step %d]: BASH %s\n", r.Step, color.YellowString(command))
+
+	bashPath := os.Getenv("SHELL")
+
+	// if not found bash in you env.
+	if len(bashPath) == 0 {
+		if bashBinPath, bashNotExist := exec.LookPath("bash"); bashNotExist != nil {
+			if shBinPath, shNotExist := exec.LookPath("sh"); shNotExist != nil {
+				return errors.New(" can not found 'bash' in your system")
+			} else {
+				bashPath = shBinPath
+			}
+		} else {
+			bashPath = bashBinPath
+		}
+	}
+
+	command = variable.Compile(command, r.Config.Var)
+
+	c := exec.Command(bashPath, "-c", command)
+
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+
+	if err := c.Run(); err != nil {
+		return err
+	}
+
+	if c.ProcessState.Success() == false {
+		return errors.New(fmt.Sprintf("run command '%s' fail.", action.Arguments))
+	}
+
+	return nil
+}
+
+func (r *Runner) actionCd(action configuration.Action) error {
+	dir := strings.Join(action.Arguments, " ")
+
+	fmt.Printf("[step %d]: CD %s\n", r.Step, color.GreenString(dir))
+
+	r.Config.CWD = variable.Compile(dir, r.Config.Var)
+
+	return nil
+}
+
+func (r *Runner) actionCmd(action configuration.Action) error {
+	fmt.Printf("[step %d]: CMD %s\n", r.Step, color.YellowString(fmt.Sprintf("%v", action.Arguments)))
+
+	command := variable.Compile(action.Arguments[0], r.Config.Var)
+	args := variable.CompileArray(action.Arguments[1:], r.Config.Var)
+
+	if _, err := exec.LookPath(command); err != nil {
+		fmt.Printf("didn't find '%s' executable\n", command)
+	}
+
+	c := exec.Command(command, args...)
+
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+
+	if err := c.Run(); err != nil {
+		return err
+	}
+
+	if c.ProcessState.Success() == false {
+		return errors.New(fmt.Sprintf("run command '%s' fail.", action.Arguments))
+	}
+
+	return nil
+}
+
+func (r *Runner) actionCopy(action configuration.Action) error {
+	sourceFilepath := variable.Compile(action.Arguments[0], r.Config.Var)
+	destinationFilepath := variable.Compile(action.Arguments[1], r.Config.Var)
+
+	fmt.Printf("[step %d]: COPY %s to %s\n", r.Step, color.YellowString(sourceFilepath), color.GreenString(destinationFilepath))
+
+	if path.IsAbs(sourceFilepath) == false {
+		sourceFilepath = path.Join(r.Config.CWD, sourceFilepath)
+	}
+
+	if path.IsAbs(destinationFilepath) == false {
+		destinationFilepath = path.Join(r.Config.CWD, destinationFilepath)
+	}
+
+	if err := r.SSH.Copy(sourceFilepath, destinationFilepath); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *Runner) actionDelete(action configuration.Action) error {
+	fmt.Printf("[step %v]: DELETE %s\n", r.Step, color.YellowString(strings.Join(action.Arguments, ",")))
+
+	args := variable.CompileArray(action.Arguments, r.Config.Var)
+
+	var files []string
+
+	for _, file := range args {
+		if path.IsAbs(file) == false {
+			file = path.Join(r.Config.CWD, file)
+		}
+
+		files = append(files, file)
+	}
+
+	if err := r.SSH.Delete(files...); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *Runner) actionDownload(action configuration.Action) error {
+	sourceFiles := variable.CompileArray(action.Arguments[:len(action.Arguments)-2], r.Config.Var)
+	destinationDir := variable.Compile(action.Arguments[len(action.Arguments)-1], r.Config.Var)
+
+	fmt.Printf("[step %d]: DOWNLOAD remote:%s to local:%s\n", r.Step, color.YellowString(strings.Join(sourceFiles, ", ")), color.GreenString(destinationDir))
+
+	if path.IsAbs(destinationDir) == false {
+		destinationDir = path.Join(r.Cwd, destinationDir)
+	}
+
+	for _, filePath := range sourceFiles {
+
+		if path.IsAbs(filePath) == false {
+			if r.Config.CWD != "" {
+				filePath = path.Join(r.Config.CWD, filePath)
+			}
+		}
+
+		err := r.SSH.Download(filePath, destinationDir)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (r *Runner) actionMove(action configuration.Action) error {
+	sourceFilepath := variable.Compile(action.Arguments[0], r.Config.Var)
+	destinationFilepath := variable.Compile(action.Arguments[1], r.Config.Var)
+
+	fmt.Printf("[step %d]: MOVE %s to %s\n", r.Step, color.YellowString(sourceFilepath), color.GreenString(destinationFilepath))
+
+	if path.IsAbs(sourceFilepath) == false {
+		sourceFilepath = path.Join(r.Config.CWD, sourceFilepath)
+	}
+
+	if path.IsAbs(destinationFilepath) == false {
+		destinationFilepath = path.Join(r.Config.CWD, destinationFilepath)
+	}
+
+	if err := r.SSH.Move(sourceFilepath, destinationFilepath); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *Runner) actionRun(action configuration.Action) error {
+	command := strings.Join(action.Arguments, " ")
+
+	fmt.Printf("[step %d]: RUN %s\n", r.Step, color.YellowString(command))
+
+	command = variable.Compile(command, r.Config.Var)
+
+	if err := r.SSH.Run(command); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *Runner) actionUpload(action configuration.Action) error {
+	sourceFiles := variable.CompileArray(action.Arguments[:len(action.Arguments)-2], r.Config.Var)
+	destinationDir := variable.Compile(action.Arguments[len(action.Arguments)-1], r.Config.Var)
+
+	fmt.Printf("[step %d]: UPLOAD local:%s to remote:%s\n", r.Step, color.YellowString(strings.Join(action.Arguments, ", ")), color.GreenString(destinationDir))
+
+	if path.IsAbs(destinationDir) == false {
+		if r.Config.CWD != "" {
+			destinationDir = path.Join(r.Config.CWD, destinationDir)
+		}
+	}
+
+	for _, filePath := range sourceFiles {
+
+		if path.IsAbs(filePath) == false {
+			filePath = path.Join(r.Cwd, filePath)
+		}
+
+		err := r.SSH.Upload(filePath, destinationDir)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (r *Runner) actionVar(action configuration.Action) error {
+	value := strings.Join(action.Arguments, " ")
+	fmt.Printf("[step %d]: VAR %s\n", r.Step, color.GreenString(value))
+
+	if Var, err := variable.Parse(value); err != nil {
+		return err
+	} else {
+		switch Var.Type {
+		case variable.TypeLiteral:
+			r.Config.Var[Var.Key] = Var.Value
+			break
+		case variable.TypeEnv:
+			if Var.Remote == false {
+				// get local env
+				r.Config.Var[Var.Key] = os.Getenv(Var.Value)
+			} else {
+				// get remote env
+				remoteEnvValue, err := r.SSH.Env(Var.Value)
+
+				if err != nil {
+					return err
+				}
+
+				r.Config.Var[Var.Key] = remoteEnvValue
+			}
+			break
+		case variable.TypeCommand:
+			if Var.Remote == false {
+				// execute command at local
+
+				arr := strings.Split(Var.Value, " ")
+				command := arr[0]
+				args := arr[1:]
+
+				c := exec.Command(command, args...)
+
+				var stdoutBuf bytes.Buffer
+				var stderrBuf bytes.Buffer
+
+				c.Stdout = &stdoutBuf
+				c.Stderr = &stderrBuf
+
+				if err := c.Run(); err != nil {
+					return err
+				}
+
+				if c.ProcessState.Success() == false {
+					return errors.New(fmt.Sprintf("run command '%s' fail.", Var.Value))
+				}
+
+				r.Config.Var[Var.Key] = strings.TrimSpace(stdoutBuf.String())
+			} else {
+				// execute command at remote
+				var stdoutBuf bytes.Buffer
+				var stderrBuf bytes.Buffer
+
+				err := r.SSH.RunRaw(Var.Value, &stdoutBuf, &stderrBuf)
+
+				if err != nil {
+					return err
+				}
+
+				r.Config.Var[Var.Key] = strings.TrimSpace(stdoutBuf.String())
+
+				fmt.Println(r.Config.Var[Var.Key])
+			}
+			break
 		}
 	}
 
