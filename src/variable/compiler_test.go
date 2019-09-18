694add02c6587d7d6f01b32f10eefcf83ccf14cc
Axetroy <axetroy.dev@gmail.com>
2019-09-18 17:26:21 +0800

    test: add test

--- src/variable/compiler_test.go
+++ src/variable/compiler_test.go
@@ -2,6 +2,7 @@ package variable_test
 
 import (
 	"github.com/axetroy/s4/src/variable"
+	"reflect"
 	"testing"
 )
 
@@ -53,6 +54,36 @@ func TestCompile(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := variable.Compile(tt.args.template, tt.args.varMap); got != tt.want {
 				t.Errorf("Compile() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCompileArray(t *testing.T) {
+	type args struct {
+		templates []string
+		varMap    map[string]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "basic",
+			args: args{
+				templates: []string{"hello {{name}}"},
+				varMap: map[string]string{
+					"name": "test",
+				},
+			},
+			want: []string{"hello test"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := variable.CompileArray(tt.args.templates, tt.args.varMap); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CompileArray() = %v, want %v", got, tt.want)
 			}
 		})
 	}
